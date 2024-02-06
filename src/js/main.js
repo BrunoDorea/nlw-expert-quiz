56739aaa35837f1e8b447f157cdd4726a4355bd5
Bruno Henrique <brunodorea@outlook.com.br>
2024-02-06 12:05:51 -0300

    :sparkles: feat: add validação das respostas + footer de acertos + css da página

--- src/js/main.js
+++ src/js/main.js
@@ -114,6 +114,10 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 
 for(const item of perguntas) {
     const quizItem = template.content.cloneNode(true)
@@ -122,6 +126,18 @@ for(const item of perguntas) {
     for(let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
         dt.querySelector('span').textContent = resposta
+        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+
+        dt.querySelector('input').onchange = (e) => {
+            const estaCorreta = e.target.value == item.correta
+
+            corretas.delete(item)
+            if(estaCorreta) {
+                corretas.add(item)
+            }
+            mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+        }
 
         quizItem.querySelector('dl').appendChild(dt)
     }
